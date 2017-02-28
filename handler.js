7f6336382ca6634742088c22e98c5a3c410a89e2
Mateus Marques <marques.m05@gmail.com>
2017-02-28 03:06:01 -0300

    criando arquivo webhook

--- handler.js
+++ handler.js
@@ -2,16 +2,12 @@
 
 module.exports.authorization = (event, context, callback) => {
 
-  console.log('Q1', event.queryStringParameters);
-  console.log('Q2', event.queryStringParameters["hub.mode"]);
-    
   if (event.queryStringParameters) {
     const mode = event.queryStringParameters["hub.mode"];
     const challenge = event.queryStringParameters["hub.challenge"];
     const verify_token = event.queryStringParameters["hub.verify_token"];
 
     if (mode === "subscribe" && verify_token === "secrettoken") {
-      // console.log("Validação OK.");
       const response = {
         statusCode: 200,
         body: challenge
@@ -29,29 +25,4 @@ module.exports.authorization = (event, context, callback) => {
 
       callback(null, response);
   }
-
-    
-
-/*
-    if (event.queryStringParameters && event.queryStringParameters.hub.mode === 'subscrible' && event.queryStringParameters.hub.verify_token === 'secret') {
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          input: event
-        })
-      };
-      
-    } else {
-      console.log('Validação falhou');
-      const response = {
-        statusCode: 401,
-        body: JSON.stringify({
-          input: event
-        })
-      };
-    }
-
-  */
-
-  
 };
