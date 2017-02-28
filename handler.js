b96fb7d47b29e4df2ff1d4ffb3823081616e3104
Mateus Marques <marques.m05@gmail.com>
2017-02-28 02:25:07 -0300

    finalizando autenticação

--- handler.js
+++ handler.js
@@ -1,19 +1,57 @@
 'use strict';
 
 module.exports.authorization = (event, context, callback) => {
-    const response = {
-      statusCode: 200,
-      body: {},
-    };
 
-    if(event.queryStringParameters && event.queryStringParameters.hub.mode === 'subscrible' && event.queryStringParameters.hub.verify_token === 'secret') {
-      response.body =  JSON.stringify( event.queryStringParameters.hub.challenge );
+  console.log('Q1', event.queryStringParameters);
+  console.log('Q2', event.queryStringParameters["hub.mode"]);
+    
+  if (event.queryStringParameters) {
+    const mode = event.queryStringParameters["hub.mode"];
+    const challenge = event.queryStringParameters["hub.challenge"];
+    const verify_token = event.queryStringParameters["hub.verify_token"];
+
+    if (mode === "subscribe" && verify_token === "secrettoken") {
+      // console.log("Validação OK.");
+      const response = {
+        statusCode: 200,
+        body: challenge
+      };
+
+      callback(null, response);
+    }
+  } else {
+    const response = {
+        statusCode: 401,
+        body: JSON.stringify({
+          input: event
+        })
+      };
+
+      callback(null, response);
+  }
+
+    
+
+/*
+    if (event.queryStringParameters && event.queryStringParameters.hub.mode === 'subscrible' && event.queryStringParameters.hub.verify_token === 'secret') {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          input: event
+        })
+      };
+      
     } else {
       console.log('Validação falhou');
-      response.statusCode = 401;
+      const response = {
+        statusCode: 401,
+        body: JSON.stringify({
+          input: event
+        })
+      };
     }
 
-  
+  */
 
-  callback(null, response);
+  
 };
